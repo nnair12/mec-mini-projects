457535cf6f2e9112a3e74a8558b5c0f9edeebe08
nikhiln <nikhiln@vmware.com>
2021-05-15 15:43:22 -0700

    Finished mini project 5.5.4

---
+++ mec-5.5.4-webscraping-project/scrapy_mini_project/scrapy_mini_project/spiders/toscrape-xpath.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class ToScrapeXPathSpider(scrapy.Spider):
+    name = 'toscrape-xpath'
+
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        for quote in response.xpath('//div[@class="quote"]'):
+            yield {
+                'text': quote.xpath('.//span[@class="text"]/text()').get(),
+                'author': quote.xpath('.//small[@class="author"]/text()').get(),
+                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').getall(),
+            }
+
+        next_page = response.xpath('//li[@class="next"]/a/@href').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
